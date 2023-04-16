3f4d58e8014e8f11c2848fef2256c39b464df3da
ZhangChuye <12110807@mail.sustech.edu.cn>
2023-04-16 19:16:35 +0800

    create another scatter python file

--- path_animation_rainbow.py
+++ path_animation_rainbow.py
@@ -11,9 +11,10 @@ import numpy as np
 
 class AnimationGenerator():
     def __init__(self):
-        self.scenario_name = 'park'  # 'campus'
+        self.scenario_name = 'campus'  # 'campus'
         self._line_color = 'royalblue'
         # self._line_color = 'royalblue'
+        self.present_checkpoints = True
         if self.scenario_name == 'park':
             # park
             self._bounding_coordinates = [113.9345,22.4904,114.0001,22.5245]
@@ -25,6 +26,9 @@ class AnimationGenerator():
 
         self.img_point_list = self.generate_points()
         self.frame = len(self.img_point_list)
+
+
+        self.check_point_list = self.get_checkpoints()
 
         
 
@@ -97,6 +101,54 @@ class AnimationGenerator():
         img_point_list = np.array(img_point_list)
         return img_point_list
 
+    def get_checkpoints(self):
+        point_path = f'{self.scenario_name}_wgs84_waypoints.pkl'
+        
+        image = plt.imread(os.path.join(
+            'background', f'{self.scenario_name}_background.jpg'))
+        
+        with open(os.path.join('checkpoints', point_path), 'rb') as f:
+            checkpoints_list = pickle.load(f)
+
+        ref_coordniate = [self._bounding_coordinates[3],
+                          self._bounding_coordinates[0]]
+        diagnal_coordinate = [
+            self._bounding_coordinates[1], self._bounding_coordinates[2]]
+
+        img_size = image.shape
+        actual_size = np.array(pm.enu.geodetic2enu(diagnal_coordinate[0], diagnal_coordinate[1], 0.0,
+                                                   ref_coordniate[0], ref_coordniate[1], 0.0,
+                                                   ell=None, deg=True)[:-1])
+        enu2img_ratio = actual_size / np.flipud(img_size)[1:]
+        print(enu2img_ratio)
+
+        img_point_list = list()
+        for checkpoint in checkpoints_list[:-2]:
+            enu_coord = np.array(pm.enu.geodetic2enu(checkpoint[0], checkpoint[1], 0.0,
+                                                     ref_coordniate[0], ref_coordniate[1], 0.0,
+                                                     ell=None, deg=True)[:-1])
+            img_point = enu_coord / enu2img_ratio
+            if len(img_point_list) == 0 or np.linalg.norm(img_point - img_point_list[-1]) >= 10:
+                img_point_list.append(img_point)
+
+        img_point_list = np.array(img_point_list)
+        return img_point_list
+
+
+    def scatter_animation(self,i):
+        areas = 35 * np.ones((i,))
+        colors = np.arange(0, i, 1)
+        scat = ax.scatter(a.img_point_list[0:i, 0], a.img_point_list[0:i, 1], s=areas, c = colors, cmap='hsv',
+                 linewidths=.4, marker='o', alpha=.75, zorder=10)
+        return scat,
+
+    def init_scatter(self):
+        # creating an empty plot/frame
+        scat = ax.scatter([],[],
+                 linewidths=.4, marker='o', alpha=.75, zorder=10)
+        return scat,
+
+
 
 if __name__ == '__main__':
 
@@ -113,11 +165,19 @@ if __name__ == '__main__':
     ax.imshow(image)
     ax.set_axis_off()
 
-    areas = 70 * np.ones((a.img_point_list.shape[0],))
-    colors = np.arange(a.img_point_list.shape[0], 0, -1)
+    # areas = 70 * np.ones((a.img_point_list.shape[0],))
+    # colors = np.arange(a.img_point_list.shape[0], 0, -1)
 
-    # ax.scatter(a.img_point_list[:, 0], a.img_point_list[:, 1], s=areas, c = colors, cmap='RdYlBu',
-                # edgecolors='k', linewidths=.8, marker='o', alpha=.75, zorder=10)
+    scat = ax.scatter([],[],
+                 linewidths=.4, marker='o', alpha=.75, zorder=10)
+
+    areas = 70 * np.ones((a.check_point_list.shape[0],))
+
+    if a.present_checkpoints:
+        ax.scatter(a.check_point_list[:, 0], a.check_point_list[:, 1], s=areas, c = 'royalblue',
+                     linewidths=.8, marker='o', alpha=.75, zorder=10)
+
+
     line, = ax.plot([], [], lw=2)
     plt.axis('off')
     plt.tight_layout(pad=0)
@@ -125,7 +185,10 @@ if __name__ == '__main__':
 
     # call the animator
     
-    anim = animation.FuncAnimation(fig, a.animate, init_func=a.init,
+    # anim = animation.FuncAnimation(fig, a.animate, init_func=a.init,
+                                #    frames=a.frame, blit=True)
+
+    anim = animation.FuncAnimation(fig, a.scatter_animation,init_func=a.init_scatter,
                                    frames=a.frame, blit=True)
 
     
